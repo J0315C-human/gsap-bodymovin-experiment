284e00cdd0fb108ba6dd945199efafff1367b318
Joel Schuman <joel@cremalab.com>
2017-10-26 16:30:14 -0500

    comment out unneeded stuff and re-add babelrc...

--- source/index.js
+++ source/index.js
@@ -1,3 +1,15 @@
+// import oweifnwoiefnw
 import './style/style.scss';
+import wef from './Bodymovin Planet Animation/bodymovin.js';
+
+
 
 console.log("HELLO TEST TEST");
+
+var animation = bodymovin.loadAnimation ({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './Bodymovin Planet Animation/planet_animation.json'
+  })

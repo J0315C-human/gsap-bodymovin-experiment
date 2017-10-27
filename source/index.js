d549c668af6ba7318aa6c33bd76f5a6e1dd32145
Joel Schuman <joel@cremalab.com>
2017-10-27 10:47:37 -0500

    changezzz

--- source/index.js
+++ source/index.js
@@ -1,15 +1,21 @@
-// import oweifnwoiefnw
 import './style/style.scss';
-import wef from './Bodymovin Planet Animation/bodymovin.js';
+import bodymovin from './Bodymovin Planet Animation/bodymovin.js';
+import './greensock-js/src/uncompressed/TweenMax';
 
+var tl = new TimelineLite({ repeat: 10 });
 
+// var planetAnimation = bodymovin.loadAnimation({
+//     container: document.getElementById('bm'),
+//     renderer: 'svg',
+//     loop: false,
+//     autoplay: true,
+//     path: './Bodymovin Planet Animation/planet_animation.json'
+// })
 
-console.log("HELLO TEST TEST");
-
-var animation = bodymovin.loadAnimation ({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './Bodymovin Planet Animation/planet_animation.json'
-  })
+// var duckAnimation = bodymovin.loadAnimation({
+//     container: document.getElementById('bm2'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: './Bodymovin Walk Cycle/duck_walking.json'
+// })
